a9d99c894a11232a870ef19a7532e4c5b8926964
Kramberger, Mitja <mitja.kramberger@gmail.com>
2015-09-02 08:47:24 +0100

    Safari 6 bugfix

--- slick/slick.js
+++ slick/slick.js
@@ -6,7 +6,7 @@
 |___/_|_|\___|_|\_(_)/ |___/
                    |__/
 
- Version: 1.5.14
+ Version: 1.5.15
   Author: Ken Wheeler
  Website: http://kenwheeler.github.io
     Docs: http://kenwheeler.github.io/slick
@@ -1743,7 +1743,7 @@
             var trackWidth = Math.ceil((_.slideWidth * _.$slideTrack.children('.slick-slide').length));
 
             (_.options.slidesGutter > 0) ?
-                _.$slideTrack.width(trackWidth + (_.options.slidesGutter * _.$slides.length)) :
+                _.$slideTrack.width(trackWidth + (_.options.slidesGutter * _.$slides.length) + _.options.slidesGutter) :
                 _.$slideTrack.width(trackWidth);
 
         } else if (_.options.variableWidth === true) {
